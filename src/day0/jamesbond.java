ae1fbb2d70a75ec3743ad527fe4f1ec9e3a7a65f
cenkbahadir <99054881+cenkbhdr@users.noreply.github.com>
2022-03-29 16:38:07 -0400

    Update jamesbond.java

--- src/day0/jamesbond.java
+++ src/day0/jamesbond.java
@@ -3,6 +3,6 @@ package day0;
 public class jamesbond{
 
 public static void main(String [] args){
-System.out.println("james bond hacked the project');
+System.out.println("james bond hacked the project");
 }
 }
